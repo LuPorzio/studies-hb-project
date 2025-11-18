490f21d384d1c7a7e553a87afd5cde3e58bc5a6f
anthony-tricarico <tricarico672@gmail.com>
2025-11-18 19:24:46 +0100

    preliminary analysis

--- eda.py
+++ eda.py
@@ -9,7 +9,9 @@ def _():
     import marimo as mo
     import numpy as np
     import pandas as pd
-    return mo, pd
+    import seaborn as sns
+    import altair as alt
+    return alt, mo, pd
 
 
 @app.cell(hide_code=True)
@@ -209,6 +211,126 @@ def _(appregress_df):
 def _(appregress_df):
     # cohort contains info about the age of users divided in groups
     appregress_df['cohort'].unique()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    We conclude this dataset contains aggregated data from the experiment that has been already elaborated for modeling. This is a step beyond the raw data as is.
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## td_ita.csv (Time Diary)
+    """)
+    return
+
+
+@app.cell
+def _(pd):
+    td_df = pd.read_csv('./data/td_ita.csv', low_memory=False)
+
+    td_df.info()
+    return (td_df,)
+
+
+@app.cell
+def _(td_df):
+    td_df.iloc[:,:]
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    1. date_not: the timestamp when the notification was received.
+    2. datein_ques: the timestamp when the question arrives.
+    3. datein_answ: the timestamp when the question is answered.
+
+    We can check how much time passes from receiving the notification and answering.
+    """)
+    return
+
+
+@app.cell
+def _(pd, td_df):
+    # convert to datetime to analyze properly
+    td_df['date_not'] = pd.to_datetime(td_df['date_not'])
+    td_df['datein_ques'] = pd.to_datetime(td_df['datein_ques'])
+    td_df['datein_answ'] = pd.to_datetime(td_df['datein_answ'])
+    return
+
+
+@app.cell
+def _(td_df):
+    # number of complete observations from the night hours
+    # check how the number of 'no information' changes throughout the day
+    plot_no_info = td_df.query("what == 'No information'")\
+        .groupby(['hh_not'])\
+        .agg(aggregated_what=('what','size')).reset_index()
+    return (plot_no_info,)
+
+
+@app.cell
+def _(alt, plot_no_info):
+    # replace _df with your data source
+    _chart = (
+        alt.Chart(plot_no_info)
+        .mark_bar()
+        .encode(
+            x=alt.X(field='hh_not', type='quantitative', title='Hour'),
+            y=alt.Y(field='aggregated_what', type='quantitative', title='# No information'),
+            tooltip=[
+                alt.Tooltip(field='hh_not', format=',.0f', title='Hour'),
+                alt.Tooltip(field='aggregated_what', format=',.0f', title='# No information')
+            ]
+        )
+        .properties(
+            title='No information over days',
+            height=290,
+            width='container',
+            config={
+                'axis': {
+                    'grid': False
+                }
+            }
+        )
+    )
+    _chart
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    We have considerably more information starting from 9 until around 23. Based on what discovered, we can drop some data to avoid including non informative units in the analysis.
+    """)
+    return
+
+
+@app.cell
+def _(td_df):
+    # remove observations from 1 am to 5 am
+    td_df_cleaned = td_df.set_index('date_not').between_time(start_time='5:00am', end_time='1:00am').reset_index()
+    td_df_cleaned.head(20)
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    So far we have cleaned the data performing these steps:
+    1. removed observations that are between 1am and 5am
+    """)
     return
 
 
